d5d2f4aadb7e8c4a4810bb9ca309e2a2681a31fa
Sayantan <131001613+Sayantan009@users.noreply.github.com>
2023-12-28 13:22:17 +0530

    project done

--- script.js
+++ script.js
@@ -7,19 +7,21 @@ const currentTime = () => {
   let hour = d.getHours()%12;
   let minutes = d.getMinutes();
   let seconds = d.getUTCSeconds()
+  
   return [hour, minutes, seconds]
 }
 
 setInterval(() => {
   let timeArr = currentTime()
+
   let hours = timeArr[0]
   let minutes =  timeArr[1]
   let seconds = timeArr[2]
+
   let hourDeg = (hours*30)-90+(minutes*.5)
   let mintDeg = -90+(minutes*6)
   let secDeg = -90+(seconds*6)
-  console.log(hours, minutes);
-  console.log(mintDeg);
+
   hourHand.style.transform = `rotate(${hourDeg}deg)`
   mintHand.style.transform = `rotate(${mintDeg}deg)`
   secHand.style.transform = `rotate(${secDeg}deg)`
